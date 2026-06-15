cc79fe01fadf0fc7cc367d8733195ea8ca426d71
Caelum Xing <caelumix.dev@pm.me>
2026-06-15 20:37:42 +0800

    feat: add LXGW WenKai Screen cask and update workflow

---
+++ Casks/font-lxgw-wenkai-mono-screen.rb
@@ -0,0 +1,10 @@
+cask "font-lxgw-wenkai-mono-screen" do
+  version "1.522"
+  sha256 "758bf0e7fceddeae425fa3f5b792aa3e80428856b9f37d21355e0e4295135e9d"
+
+  url "https://github.com/lxgw/LxgwWenKai-Screen/releases/download/v#{version}/LXGWWenKaiMonoScreen.ttf"
+  name "LXGW WenKai Mono Screen"
+  homepage "https://github.com/lxgw/LxgwWenKai-Screen"
+
+  font "LXGWWenKaiMonoScreen.ttf"
+end
---
+++ Casks/font-lxgw-wenkai-screen.rb
@@ -0,0 +1,10 @@
+cask "font-lxgw-wenkai-screen" do
+  version "1.522"
+  sha256 "cd1a6fa39c4ea42fd8f4e289945789b0e510cf7016435640f8893cdad9b220f3"
+
+  url "https://github.com/lxgw/LxgwWenKai-Screen/releases/download/v#{version}/LXGWWenKaiScreen.ttf"
+  name "LXGW WenKai Screen"
+  homepage "https://github.com/lxgw/LxgwWenKai-Screen"
+
+  font "LXGWWenKaiScreen.ttf"
+end
