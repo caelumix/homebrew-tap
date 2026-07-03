ffc35ab797b35571a8f28bc2f80a26a5df9e587b
Caelum Xing <caelumix.dev@pm.me>
2026-07-03 17:59:14 +0800

    feat: add Yuniversus font cask and update workflow

---
+++ Casks/font-yuniversus.rb
@@ -0,0 +1,10 @@
+cask "font-yuniversus" do
+  version "3.11.0"
+  sha256 "e70a8c295345af92fb39f7a976f16f5f42ca86eba602da87da743df94b7acba8"
+
+  url "https://github.com/forfudan/yuhao-ime-release/releases/download/v#{version}/riyue_lingming_v#{version}.zip"
+  name "Yuniversus"
+  homepage "https://github.com/forfudan/yuhao-ime-release"
+
+  font "Yuniversus.ttf"
+end
